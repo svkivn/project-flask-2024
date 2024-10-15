d4cc0146238b07ab656be5b8b6f30776edb6aa50
Ivan <user-email@go.uk>
2024-10-15 18:09:16 +0300

    add config

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, url_for
-app = Flask(__name__)    
+app = Flask(__name__)
+app.config.from_pyfile("config.py")
 
 @app.route('/')   # URL '/' to be handled by main() route handler
 def main():
@@ -23,9 +24,6 @@ def admin():
     to_url = url_for("greetings", name="administrator", _external=True)    # --> "http://localhost:8080/hi/administrator"
     print(to_url)
     return redirect(to_url)
-
-
-
 
 if __name__ == "__main__":
     app.run()  # Launch built-in web server and run this Flask webapp, debug=True
