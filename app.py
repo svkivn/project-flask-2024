eb985cf7ebb1f66e0f577cfc6d84c34d6c2c6a9d
Ivan <user-email@go.uk>
2024-10-15 17:50:40 +0300

    add dynamic route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask  
+from flask import Flask, request
 app = Flask(__name__)    
 
 @app.route('/')   # URL '/' to be handled by main() route handler
@@ -8,7 +8,16 @@ def main():
 @app.route('/homepage') 
 def home():
     """View for the Home page of your website."""
-    return "This is your homepage :) "
+    agent = request.user_agent
+    return f"<h1>This is your homepage :) - {agent}</h1> "
+
+@app.route('/hi/<string:name>')  # /hi/ivan?age=30
+def greetings(name):
+    name = name.upper()
+    age = request.args.get("age", 0, type=int)
+
+    return f"Welcome {name=} {age=}"
+
 
 if __name__ == "__main__":
     app.run()  # Launch built-in web server and run this Flask webapp, debug=True
