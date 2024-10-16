dad61c0e91204b2843413d899726989b3eafbf9e
Ivan <user-email@go.uk>
2024-10-16 17:26:38 +0300

    add first template

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 @app.route('/')
 def main():
-    return '<h1>Hello, world!</h1>', 200
+    return render_template("hello.html")
 
 @app.route('/homepage') 
 def home():
     """View for the Home page of your website."""
     agent = request.user_agent
 
-    return f"This is your homepage :) - {agent} "
+    return render_template("home.html", agent=agent)
 
 @app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
 def greetings(name):
