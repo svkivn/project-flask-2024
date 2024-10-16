33239d9567d33950783a2b60066dc5942ca13381
Ivan <user-email@go.uk>
2024-10-16 16:38:08 +0300

    add config.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 
-app = Flask(__name__)    
+app = Flask(__name__)
+app.config.from_pyfile("config.py")
 
 @app.route('/')
 def main():
@@ -9,8 +10,24 @@ def main():
 @app.route('/homepage') 
 def home():
     """View for the Home page of your website."""
+    agent = request.user_agent
 
-    return "This is your homepage :) "
+    return f"This is your homepage :) - {agent} "
+
+@app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
+def greetings(name):
+    name = name.upper()
+    age = request.args.get("age", 0, int)
+    year = 2024 - age
+
+    return f"Welcome, {name}  {year}"
+
+@app.route("/admin")
+def admin():
+    to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
+    print(to_url)
+    return redirect(to_url)
+
 
 if __name__ == "__main__":
     app.run()  # Launch built-in web server and run this Flask webapp, debug=True
