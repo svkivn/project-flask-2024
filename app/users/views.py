6a93c83c22f6b7e9909405a4dd8445d0be98c670
Ivan <user-email@go.uk>
2024-10-30 17:21:24 +0200

    add cookie

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,6 @@
 from . import bp
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, make_response
+from datetime import timedelta, datetime
 
 @bp.route("/hi/<string:name>")   #/hi/ivan?age=45
 def greetings(name):
@@ -14,3 +15,22 @@ def admin():
     to_url = url_for("user_name.greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
     print(to_url)
     return redirect(to_url)
+
+
+@bp.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кука встановлена')
+    response.set_cookie('username', 'student', max_age=timedelta(seconds=60))
+    response.set_cookie('color', '', max_age=timedelta(seconds=60))
+    return response
+
+@bp.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return f'Користувач: {username}'
+
+@bp.route('/delete_cookie')
+def delete_cookie():
+    response = make_response('Кука видалена')
+    response.set_cookie('username', '', expires=0) # response.set_cookie('username', '', max_age=0)
+    return response
