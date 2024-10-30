e4a6934e4fbd927957b80ecd9955962f8926a8e1
Ivan <user-email@go.uk>
2024-10-30 18:18:52 +0200

    add session and message

--- app/users/views.py
+++ app/users/views.py
@@ -1,6 +1,32 @@
 from . import bp
-from flask import render_template, redirect, request, url_for, make_response
+from flask import render_template, redirect, request, url_for, make_response, session, flash
 from datetime import timedelta, datetime
+
+@bp.route("/profile")
+def get_profile():
+    if "username" in session:
+        username_value = session["username"]
+        return render_template("profile.html", username=username_value)
+    flash("Invalid: Session.", "danger")
+    return redirect(url_for("user_name.login"))
+
+@bp.route("/login",  methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form["login"]
+        session["username"] = username
+        flash("Success: session added successfully.", "success")
+        return redirect(url_for("user_name.get_profile"))
+    return render_template("login.html")
+
+@bp.route('/logout')
+def logout():
+    # Видалення користувача із сесії
+    session.pop('username', None)
+    session.pop('age', None)
+    return redirect(url_for('user_name.get_profile'))
+
+
 
 @bp.route("/hi/<string:name>")   #/hi/ivan?age=45
 def greetings(name):
