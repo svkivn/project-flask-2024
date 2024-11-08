9b2bd981d6e98fa0ac31aad5446126f077005adf
Ivan <user-email@go.uk>
2024-11-08 10:07:30 +0200

    add wtf-form

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,11 +1,23 @@
 from . import post_bp
-from flask import render_template, abort
+from flask import render_template, abort, flash, url_for, redirect
+from .forms import PostForm
 
 posts = [
     {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
     {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
     {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
-] 
+]
+
+@post_bp.route('/add_post', methods=["GET", "POST"]) 
+def add_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        #обробка логіки
+        flash(f"Post {title} added successfully!", "success")
+        return redirect(url_for(".get_posts"))
+
+    return render_template("add_post.html", form=form)
 
 @post_bp.route('/') 
 def get_posts():
