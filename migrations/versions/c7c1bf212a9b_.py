815b7622619d70bbb239f48b41131cc76a7f0e6b
Ivan <user-email@go.uk>
2024-12-03 10:19:12 +0200

    fix head in migrations

---
+++ migrations/versions/c7c1bf212a9b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c7c1bf212a9b
+Revises: 39c189a8b3c9, b4fe7fa08c01
+Create Date: 2024-12-03 10:17:22.687944
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c7c1bf212a9b'
+down_revision = ('39c189a8b3c9', 'b4fe7fa08c01')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
